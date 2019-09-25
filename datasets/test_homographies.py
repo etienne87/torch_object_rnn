1cdd6390017507e85b8bcc5098ea24c78c86d318
etienne87 <et.perot@gmail.com>
2019-09-25 22:36:40 +0200

    show voyage

--- datasets/test_homographies.py
+++ datasets/test_homographies.py
@@ -34,24 +34,37 @@ def generate_homography(rvec1, tvec1, rvec2, tvec2, nt, K, Kinv, d):
     return G_0to2
 
 
-if __name__ == '__main__':
-    img = cv2.imread("datasets/scene.jpg", cv2.IMREAD_GRAYSCALE)
-    height, width = img.shape
+class PlanarVoyage(object):
+    def __init__(self, height, width):
+        self.K = np.array([[width / 2, 0, width / 2],
+                      [0, width / 2, height / 2],
+                      [0, 0, 1]], dtype=np.float32)
+        self.Kinv = np.linalg.inv(self.K)
 
-    K = np.array([[width / 2, 0, width / 2],
-                  [0, width/2, height / 2],
-                  [0, 0, 1]], dtype=np.float32)
-    Kinv = np.linalg.inv(K)
+        self.rvec1 = np.array([0, 0, 0], dtype=np.float32)
+        self.tvec1 = np.array([0, 0, 0], dtype=np.float32)
+        self.nt = np.array([0, 0, -1], dtype=np.float32).reshape(1, 3)
 
-    rvec1 = np.array([0,0,0], dtype=np.float32)
-    tvec1 = np.array([0,0,0], dtype=np.float32)
-    nt = np.array([0,0,-1], dtype=np.float32).reshape(1,3)
+        self.rvec_speed = np.random.rand(3) * 0.25
+        self.tvec_speed = np.random.rand(3) * 0.5
+        self.tshift = np.random.randn(3)
+        self.d = 1
+        self.time = 0
+
+    def __call__(self):
+        tshift = moving_average(self.tshift, 1e-4)
+        rvec2 = self.rvec_speed * np.sin(self.time * 0.01)
+        tvec2 = self.tvec_speed * np.sin(self.time * 0.01 + tshift)
+        G_0to2 = generate_homography(self.rvec1, self.tvec1, rvec2, tvec2, self.nt, self.K, self.Kinv, self.d)
+
+        self.time += 1
+        return G_0to2
 
 
+def show_voyage(img):
+    height, width = img.shape[:2]
 
-    rvec_speed = np.random.rand(3) * 0.25
-    tvec_speed = np.random.rand(3) * 0.5
-    tshift = np.random.randn(3)
+    voyage = PlanarVoyage(height, width)
 
     prev_img = img.astype(np.float32)
 
@@ -59,29 +72,42 @@ if __name__ == '__main__':
     t = 0
 
     while 1:
-        tshift = moving_average(tshift, 1e-4)
 
-
-        rvec2 = rvec_speed * np.sin(t*0.01)
-        tvec2 = tvec_speed * np.sin(t*0.01 + tshift)
-
-
-        G_0to2 = generate_homography(rvec1, tvec1, rvec2, tvec2, nt, K, Kinv, d)
-
-
+        G_0to2 = voyage()
 
         out = cv2.warpPerspective(img, G_0to2, (width, height),
                                   flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REFLECT).astype(np.float32)
-        out = (out-out.min())/(out.max()-out.min())
+        out = (out - out.min()) / (out.max() - out.min())
 
-        diff = out-prev_img
-        diff = (diff-diff.mean())/(1e-3 + diff.std())
-        diff = (diff-diff.min())/(diff.max()-diff.min())
+        diff = out - prev_img
+
+        # Remove some events
+        diff *= np.random.rand(height, width, 3) < 0.9
+
+        diff = (diff - diff.mean()) / (1e-3 + diff.std())
+        diff = diff.clip(diff.mean() - 3 * diff.std(), diff.mean() + 3 * diff.std())
+        diff = (diff - diff.min()) / (diff.max() - diff.min())
+
+        # Salt-and-Pepper
+        # diff += (np.random.rand(height, width)[:,:,None].repeat(3,2) < 0.00001)/2
+        # diff -= (np.random.rand(height, width)[:,:,None].repeat(3,2) < 0.00001) / 2
 
         cv2.imshow("diff", diff)
         cv2.imshow("out", out)
-        cv2.waitKey(5)
-
+        key = cv2.waitKey(5)
+        if key == 27:
+            break
         prev_img = out
 
         t += 1
+
+
+if __name__ == '__main__':
+    import glob
+    imgs = glob.glob("/home/etienneperot/workspace/data/coco/train2017/"+"*.jpg")
+
+
+    while 1:
+        imfile = imgs[np.random.randint(len(imgs))]
+        img = cv2.imread(imfile)
+        show_voyage(img)
