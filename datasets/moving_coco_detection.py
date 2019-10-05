83db8e4f899baa8b76b52b9960d2882da2ae9ad0
etienne87 <et.perot@gmail.com>
2019-10-05 20:08:24 +0200

    coco ready

--- datasets/moving_coco_detection.py
+++ datasets/moving_coco_detection.py
@@ -3,8 +3,10 @@ from __future__ import division
 from __future__ import print_function
 
 import os
+import time
 import numpy as np
 
+import torch
 from torch.utils.data import Dataset
 from pycocotools.coco import COCO
 
@@ -37,14 +39,17 @@ def discard_too_small(boxes, min_size=30):
     return boxes[ids]
 
 
-def boxarray_to_boxes(boxes):
-    bboxes = []
-    for i, box in enumerate(boxes):
-        pt1 = (box[0], box[1])
-        pt2 = (box[2], box[3])
-        bb = ('', i, pt1, pt2, None, None, None)
-        bboxes.append(bb)
-    return bboxes
+def wrap_boxes(boxes, height, width):
+    allbox = [boxes]
+    for y in [-height, 0, height]:
+        for x in [-width, 0, width]:
+            if x == 0 and y == 0:
+                continue
+            shift = boxes.copy()
+            shift[:, [0, 2]] += x
+            shift[:, [1, 3]] += y
+            allbox.append(shift)
+    return np.concatenate(allbox, 0)
 
 
 
@@ -63,7 +68,8 @@ class MovingCOCODataset(Dataset):
         self.height = height
         self.width = width
         self.cat2label = {cat_id: i for i, cat_id in enumerate(self.catIds)}
-        self.img_warp = partial(cv2.warpPerspective, dsize=(width, height), borderMode=cv2.BORDER_WRAP)
+        self.border_mode = cv2.BORDER_WRAP
+        self.img_warp = partial(cv2.warpPerspective, dsize=(width, height), borderMode=self.border_mode)
 
     def _getImgIdsUnion(self, catIds):
         out = []
@@ -91,11 +97,9 @@ class MovingCOCODataset(Dataset):
         anns = self.coco.loadAnns(annIds)
         boxes = self.get_boxes(anns)
 
+        image, boxes = resize_image_and_boxes(image, boxes, (self.width, self.height))
         imseq, boxseq = self.trip(image, boxes, self.time)
-        return imseq
-
-        # img, boxes = resize_image_and_boxes(image, boxes, (self.width, self.height))
-        # return img, boxes.astype(np.int32)
+        return imseq, boxseq
 
     def __len__(self):
         return len(self.imgIds)
@@ -103,33 +107,69 @@ class MovingCOCODataset(Dataset):
     def trip(self, image, boxes, tbins):
         height, width = image.shape[:2]
         voyage = imutil.PlanarVoyage(height, width)
-
+        if self.border_mode == cv2.BORDER_WRAP:
+            boxes = wrap_boxes(boxes, height, width)
         imseq = []
         boxseq = []
         for _ in range(tbins):
             G_0to2 = voyage()
-            out = self.img_warp(image, G_0to2, flags=cv2.INTER_LINEAR).astype(np.float32)
-            tboxes = imutil.cv2_apply_transform_boxes(boxes, G_0to2)
+            out = self.img_warp(image, G_0to2, flags=cv2.INTER_LINEAR).astype(np.float32)/128 - 1
+            labels = boxes[:, 4:5]
+            tboxes = imutil.cv2_apply_transform_boxes(boxes[:, :4], G_0to2)
             tboxes = clamp_boxes(tboxes, height, width)
+            tboxes = np.concatenate([tboxes, labels], 1)
             tboxes = discard_too_small(tboxes, 10)
-            imseq.append(out)
+            out = torch.from_numpy(out).permute([2, 0, 1])
+            tboxes = torch.from_numpy(tboxes.astype(np.int32))
+            imseq.append(out[None])
             boxseq.append(tboxes)
+        imseq = torch.cat(imseq, 0)
         return imseq, boxseq
+
+def collate_fn(data_list):
+    videos, boxes = zip(*data_list)
+    videos = torch.stack(videos, 1)
+    t, n = videos.shape[:2]
+    boxes = [[boxes[i][t] for i in range(n)] for t in range(t)]
+    return videos, boxes
+
+
+def viz_batch(seq_im, seq_boxes):
+    video = seq_im.permute([0, 2, 3, 1]).cpu().numpy() * 127 + 127
+    video = np.split(video, 10)
+    seq_boxes = [item.cpu().numpy().copy() for item in seq_boxes]
+    for im, boxes in zip(video, seq_boxes):
+        im = im[0].astype(np.uint8).copy()
+        bboxes = vis.boxarray_to_boxes(boxes, boxes[:, -1], mcoco.catNms)
+        img_ann = vis.draw_bboxes(im, bboxes, 2, colormap=cv2.COLORMAP_JET)
+        cv2.imshow('im', img_ann)
+        cv2.waitKey(5)
+
 
 if __name__ == '__main__':
     dataDir = '/home/etienneperot/workspace/data/coco'
     dataType = 'val2017'
     annFile = '{}/annotations/instances_{}.json'.format(dataDir, dataType)
 
-    mcoco = MovingCOCODataset(annFile)
+    mcoco = MovingCOCODataset(annFile, time=10)
+    loader = torch.utils.data.DataLoader(mcoco, batch_size=10, num_workers=3,
+                           shuffle=True, collate_fn=collate_fn, pin_memory=True)
 
+    # while 1:
+    #     seq_im, seq_boxes = mcoco[np.random.randint(0, len(mcoco))]
+    #     viz_batch(seq_im, seq_boxes)
 
-    while 1:
-        seq_im, seq_boxes = mcoco[np.random.randint(0, len(mcoco))]
+    start = time.time()
+    for x, y in loader:
 
-        for im, boxes in zip(seq_im, seq_boxes):
-            bboxes = vis.boxarray_to_boxes(boxes, boxes[:, -1], mcoco.catNms)
-            img_ann = vis.draw_bboxes(im, bboxes, 2, colormap=cv2.COLORMAP_JET)
+        end = time.time()
+        print(end - start, ' time loading')
+        # for i in range(10):
+        #     video = x[:, i]
+        #     seq_boxes = [item[i] for item in y]
+        #     viz_batch(video, seq_boxes)
 
-            cv2.imshow('im', im)
-            cv2.waitKey()
+        time.sleep(0.5)
+
+        start = time.time()
+        print(start-end, ' time showing')
