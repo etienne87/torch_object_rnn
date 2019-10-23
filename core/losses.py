e87d8620fa6784377b26a4bef366673684af6fe5
etienne87 <et.perot@gmail.com>
2019-10-23 09:00:25 +0200

    losses modules proper implementation separated

---
+++ core/losses.py
@@ -0,0 +1,111 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def reduce(loss, mode='none'):
+    ''' reduce mode
+
+    :param loss:
+    :param mode:
+    :return:
+    '''
+    if mode == 'mean':
+        loss = loss.mean()
+    elif mode == 'sum':
+        loss = loss.sum()
+    return loss
+
+
+def softmax_focal_loss(x, y, reduction='none'):
+    ''' softmax focal loss
+
+    :param x: [N, A, C+1]
+    :param y: [N, A]
+    :param reduction:
+    :return:
+    '''
+    gamma = 2.0
+    r = torch.arange(x.size(0))
+    ce = F.log_softmax(x, dim=1)[r, y]
+    pt = torch.exp(ce)
+    weights = (1-pt).pow(gamma)
+    loss = -(weights * ce)
+    loss[y < 0] = 0
+    return reduce(loss, reduction)
+
+
+def sigmoid_focal_loss(x, y, reduction='none'):
+    ''' sigmoid focal loss
+
+    :param x: [N, A, C]
+    :param y: [N, A]
+    :param reduction:
+    :return:
+    '''
+    alpha = 0.25
+    gamma = 2.0
+    s = x.sigmoid()
+    batchsize, num_anchors, num_classes = x.shape
+
+    t = torch.zeros((len(x), num_classes), dtype=pred.dtype, device=pred.device)
+    t[:, y] = y > 0 #at right column put a 1 or a zero if bg
+
+    pt = (1 - s) * t + s * (1 - t)
+    focal_weight = (alpha * t + (1 - alpha) *
+                    (1 - t)) * pt.pow(gamma)
+    loss = F.binary_cross_entropy_with_logits(
+        x, target, reduction='none') * focal_weight
+    loss = loss.sum(dim=-1)
+    loss[y < 0] = 0
+
+    loss = reduce(loss, reduction)
+    return loss
+
+
+def smooth_l1_loss(loc_preds, loc_targets, pos):
+    ''' compute regression only where pos
+
+    :param loc_preds:
+    :param loc_targets:
+    :param pos:
+    :return:
+    '''
+    mask = pos.unsqueeze(2).expand_as(loc_preds)  # [N,#anchors,4]
+    loc_loss = F.smooth_l1_loss(loc_preds[mask], loc_targets[mask], reduction='sum')
+    loc_loss /= num_pos
+    return loc_loss
+
+
+def ohem_loss(cls_preds, pos):
+    ''' hard-negative mining
+
+    :param cls_preds:
+    :param pos:
+    :param neg:
+    :return:
+    '''
+    num_pos = pos.sum().item()
+
+    cls_loss = F.cross_entropy(cls_preds.view(-1, cls_preds.size(-1)),
+                               cls_targets.view(-1), ignore_index=-1, reduction='none')
+    cls_loss = cls_loss.view(batch_size, -1)
+
+    cls_loss2 = cls_loss * (pos.float() - 1)
+    _, idx = cls_loss2.sort(1)  # sort by negative losses
+    _, rank = idx.sort(1)  # [N,#anchors]
+    num_neg = 3 * pos.sum(1)  # [N,]
+    neg = rank < num_neg[:, None]
+    cls_loss = cls_loss[pos | neg].sum()
+    cls_loss /= num_pos
+    return cls_loss
+
+
+
+#
+# class DetectionLoss(nn.Module):
+#     def __init__(self):
