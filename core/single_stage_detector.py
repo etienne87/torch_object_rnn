3123df21f310000b3d6adc11ed1ce5f85a936014
etienne87 <et.perot@gmail.com>
2019-10-29 12:10:35 +0100

    sigmoid focal loss set in single_stage_detector

--- core/single_stage_detector.py
+++ core/single_stage_detector.py
@@ -33,7 +33,7 @@ class SingleStageDetector(nn.Module):
 
         self.rpn = rpn(self.feature_extractor.cout, self.box_coder.num_anchors, self.num_classes + self.label_offset, act)
 
-        self.criterion = DetectionLoss()
+        self.criterion = DetectionLoss('sigmoid_focal_loss')
 
     def reset(self):
         self.feature_extractor.reset()
